814b426ea7debc817cb3ac3861f7eec31435587f
Michael Bestas <mkbestas@lineageos.org>
2019-09-26 01:56:06 +0300

    land: sepolicy: Switch to device/qcom/sepolicy-legacy-um policy
    Change-Id: I09ee12755ab0a1f54a9dca4b92313ef1127db3d6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,7 +187,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 VENDOR_SECURITY_PATCH := 2020-08-01
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
