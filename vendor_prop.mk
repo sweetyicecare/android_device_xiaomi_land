c699b9c3cfec9938f9aaff04ee8b89c25fee9e87
LuK1337 <priv.luk@gmail.com>
2020-01-14 19:02:40 +0100

    land: prop: Enable ro.telephony.use_old_mnc_mcc_format
    Change-Id: I46e510d35ad0cf2befebbf3e1530e30a2f2e4f71

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -132,6 +132,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=dsds \
     ro.telephony.default_network=9,1 \
     ro.telephony.call_ring.multiple=false \
+    ro.telephony.use_old_mnc_mcc_format=true
     persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
