e661489f1091ced1afd8d79a07745c2d0d5ebff5
TingyiChen <isaacchen@isaacchen.cn>
2020-04-23 19:26:48 +0700

    land: Disable camera gyro
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- prop.mk
+++ prop.mk
@@ -1,5 +1,5 @@
 #
-# system.prop for mido
+# system.prop for land
 #
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -48,7 +48,7 @@ camera.display.lmax=1280x720 \
 camera.display.umax=1920x1080 \
 camera.lowpower.record.enable=1 \
 media.camera.ts.monotonic=1 \
-persist.camera.gyro.disable=0 \
+persist.camera.gyro.disable=1 \
 persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
