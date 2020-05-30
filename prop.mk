940a91f6cdeaf8174d4df0ae1777fdcb32391847
LuK1337 <priv.luk@gmail.com>
2020-05-30 18:00:45 +0200

    land: Drop qti-telephony-common aosp_usr_pref_sel
    * Seems like mobile data doesn't get toggled
      properly after doing this...
    
    Change-Id: I1ba996aec5a80bd7de8c1bddbd4a2a33b8146e57

--- prop.mk
+++ prop.mk
@@ -167,7 +167,6 @@ persist.vendor.radio.custom_ecc=1 \
 persist.vendor.radio.hw_mbn_update=0 \
 persist.vendor.radio.rat_on=combine \
 persist.vendor.radio.sib16_support=1 \
-persist.vendor.radio.aosp_usr_pref_sel=true \
 persist.vendor.data.iwlan.enable=true \
 persist.dbg.ims_volte_enable=1 \
 ril.subscription.types=NV,RUIM \
