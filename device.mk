62845b038fe878b3bae742499dcf5f868631d13e
zeelog <zeelogkang@gmail.com>
2020-11-08 22:59:51 +0000

    land: Build light binderized HAL
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- device.mk
+++ device.mk
@@ -249,7 +249,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_mido
+    android.hardware.light@2.0-service.xiaomi_land
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
