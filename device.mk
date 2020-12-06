02586511966d41852193f95a904d7ebcba3a27cd
KakatkarAkshay <cryllicbuster273@pixelexperience.org>
2020-12-06 16:52:52 +0100

    land: Build thermal hal from source
    Change-Id: I9c4583c838ca963f2b3b3d5acc36cf0313594db4

--- device.mk
+++ device.mk
@@ -405,7 +405,8 @@ PRODUCT_BOOT_JARS += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    thermal.msm8937
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
