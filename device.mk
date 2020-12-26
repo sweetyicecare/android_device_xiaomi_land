43d3ff300e41842a6066d56e3b67bc2c84379703
codeworkx <daniel.hillenbrand@codeworkx.de>
2020-12-26 22:57:01 +0700

    land: Build android.hardware.radio 1.2 and 1.4
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- device.mk
+++ device.mk
@@ -335,6 +335,8 @@ PRODUCT_PACKAGES += \
     libxml2
 
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.2 \
+    android.hardware.radio@1.4 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0
 
