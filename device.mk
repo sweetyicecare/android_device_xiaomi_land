f111568d4d5cc3858eec09aa78c32fe474143267
Arne Coucheron <arco68@gmail.com>
2021-01-23 11:35:56 +0400

    land: Use in-process APK
    For : Java, Network, Wifi, Compiler, APK
    Taken from common go configuration, and the purpose is
    to save some memory.
    
    Change-Id: I32222daf8fa2be1f41a8177b82fa6f2f94e002e7
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -463,5 +463,19 @@ PRODUCT_COPY_FILES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# Do not spin up a separate process for the network stack, use an in-process APK.
+PRODUCT_PACKAGES += InProcessNetworkStack
+PRODUCT_PACKAGES += com.android.tethering.inprocess
+
+# Reduce system image size by limiting java debug info.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
+# Always preopt extracted APKs to prevent extracting out of the APK
+# for gms modules.
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
 # Inherit device proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/land/land-vendor.mk)
