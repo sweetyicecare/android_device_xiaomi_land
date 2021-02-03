b91bb930512a9b63fc15871fab8c9f8f9e2ccb94
akhilnarang <me@akhilnarang.dev>
2021-02-03 19:42:31 +0700

    land: Inherrit Google Play System Update
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- aosip_land.mk
+++ aosip_land.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 # Inherit some common AOSiP stuff
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
+# Inherit Google Play System Update stuff
+$(call inherit-product-if-exists, vendor/google/psu/google-psu.mk)
+
 # Inherit from land device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
