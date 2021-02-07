928cb7ed0880b5dac7f391e7fd3d1d28aac8166c
JacobDeepu <d2jdeepu@gmail.com>
2021-02-07 17:27:53 +0700

    land: sepolicy: Update gx_fpd to use vndbinder
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -190,7 +190,6 @@ VENDOR_SECURITY_PATCH := 2020-08-01
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 BOARD_VNDK_VERSION := current
