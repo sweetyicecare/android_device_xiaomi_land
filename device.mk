d091cbdac5a00956aa5af6cb8659a7db92441c39
Pranav Vashi <neobuddy89@gmail.com>
2021-05-10 22:09:22 +0200

    land: Build 32-bit android.frameworks.displayservice@1.0

--- device.mk
+++ device.mk
@@ -120,6 +120,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service
 
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0
