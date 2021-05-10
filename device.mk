f6e80d4eb86d26d865bab9589d7841e9da90615a
erfanoabdi <erfangplus@gmail.com>
2021-05-10 03:07:29 +0000

    land: Build QTI thermal hal
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- device.mk
+++ device.mk
@@ -426,6 +426,9 @@ PRODUCT_PACKAGES += \
 
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
